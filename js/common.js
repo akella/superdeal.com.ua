e5eeb2950dbf7acb50e3aad68d492d1b822bd937
akella <akella.a@gmail.com>
2013-02-06 12:45:50 +0200

    footer header and content area

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-
+	$('.city__change').click(function(event) {
+		$('.location').slideToggle();
+		return false;
+	});
+	$('.profile__user').click(function(event) {
+		$('.profile__drop').fadeToggle('fast');
+		return false;
+	});
 });
