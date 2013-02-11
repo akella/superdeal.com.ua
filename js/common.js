5352b4acbcc50586fb64972f9dc5f4c50e1afd96
akella <akella.a@gmail.com>
2013-02-11 13:24:53 +0200

    main done

--- js/common.js
+++ js/common.js
@@ -7,4 +7,20 @@ $(document).ready(function() {
 		$('.profile__drop').fadeToggle('fast');
 		return false;
 	});
+
+	$('.pics__wrap').each(function(event) {
+		le = $(this).children().length;
+		if(le>1){
+			$(this).parent().addClass('pics_slider');
+			var prev_arr = $(this).parent().children('.pics__prev');
+	        var next_arr = $(this).parent().children('.pics__next');;
+	        $(this).cycle({
+	            fx:      'scrollHorz',
+	            next:    next_arr,
+	            prev:    prev_arr,
+	            timeout:  0,
+	            speed: 700
+	        });
+		}
+	});
 });
